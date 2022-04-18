486c85bd975c1abf6831b20542f9d90b0b75074b
Marcus Kjell <marcus.kjell@gmail.com>
2022-04-18 22:45:03 +0200

    prisma added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,35 @@ import Gallery from "@/components/Gallery";
 import { getMovies } from "@/shared/api";
 import { TmdbMovieItem } from "@/shared/models/tmdb";
 import { GetServerSideProps, NextPage } from "next";
+import { prisma } from "@/db/client";
+import { Movie } from "@prisma/client";
 
+// export const getServerSideProps: GetServerSideProps = async (_context) => {
+//   const movies = await getMovies();
+//   return {
+//     props: {
+//       movies,
+//     },
+//   };
+// };
 export const getServerSideProps: GetServerSideProps = async (_context) => {
-  const movies = await getMovies();
+  const movies = await prisma.movie.findMany();
   return {
     props: {
-      movies,
+      movies: JSON.stringify(movies),
     },
   };
 };
 
 interface IndexProps {
-  movies: TmdbMovieItem[];
+  movies: string;
 }
 
 const Index: NextPage<IndexProps> = ({ movies }) => {
   return (
     <div className="my-6">
-      <Gallery movies={movies} />
+      {/* <Gallery movies={movies} /> */}
+      <code>{movies}</code>
     </div>
   );
 };
