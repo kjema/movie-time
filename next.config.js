79296d773f8c25aeca5ab659d2426ee47a2bbd46
Marcus Kjell <marcus.kjell@gmail.com>
2022-09-09 21:42:55 +0200

    Update dependencies & nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org"],
   },
+  swcMinify: true,
+  poweredByHeader: false,
+  output: "standalone",
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+  },
 };
+
+module.exports = nextConfig;
